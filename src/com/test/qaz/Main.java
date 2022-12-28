87e2fc4a2ced3a37d378eed41568535ce5ad2028
gestina <gestina@inbox.ru>
2022-12-28 14:38:30 +0600

    firstTask

--- src/com/test/qaz/Main.java
+++ src/com/test/qaz/Main.java
@@ -1,4 +1,15 @@
 package com.test.qaz;
 
 public class Main {
+    public static void main(String[] args) {
+        String randomText = new String("Я принимаю произвольный текст, и делю строку на Абзацы." +
+                "Так-же, я вывожу кол-во слов в этом абзаце.");
+        System.out.println("Тест");
+        for (String degree : randomText.split(",")) {
+            System.out.println(degree);
+        }
+    }
+    public static void main2() {
+
+    }
 }
