7cd4fc429367e13668acc023c5200e761123fa07
gestina <gestina@inbox.ru>
2022-12-28 16:47:44 +0600

    firstTask2

---
+++ src/com/test/qaz/FirstTask2.java
@@ -0,0 +1,14 @@
+package com.test.qaz;
+
+import java.util.StringTokenizer;
+
+public class FirstTask2 {
+    public static void main(String[] args) {
+        StringTokenizer stringTokenizer = new StringTokenizer("Я принимаю произвольный текст,и делю строку на Абзацы," +
+                "Так-же я вывожу кол-во слов в этом абзаце.", ",");
+
+        int count = stringTokenizer.countTokens();
+        System.out.println("Всего абзацев: " + count);
+
+    }
+}
